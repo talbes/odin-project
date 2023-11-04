1c8ded0c212856aa28313ab64632751d9b7f70d7
Tim Albes <tim.albes@gmail.com>
2023-11-04 17:34:40 +0100

    Add shopping list with possibility to remove items.

--- 3-javascript-fundamentals/dom-manipulation.js
+++ 3-javascript-fundamentals/dom-manipulation.js
@@ -1,10 +1,10 @@
-const testingDom = document.body.querySelector("h2");
+const testingDom = document.querySelector("body");
 
 const newHeading = document.createElement("h3");
 newHeading.textContent = "I'm a blue h3!"
 newHeading.setAttribute("style", "color: blue");
 newHeading.setAttribute("id", "newH");
-testingDom.insertAdjacentElement('afterend',newHeading);
+testingDom.insertAdjacentElement('beforeend',newHeading);
 
 const newPara = document.createElement("p");
 newPara.textContent = "Hey I'm red!";
@@ -23,3 +23,24 @@ document.querySelector("#newDiv").appendChild(anotherP);
 document.querySelector("#newDiv").appendChild(anotherD);
 anotherP.textContent = "I'm a p";
 anotherD.textContent = "I'm a d";
+
+// Shopping list
+let addItemBtn = document.querySelector("#btn-add-to-cart");
+let itemToAdd = document.querySelector("#item");
+addItemBtn.addEventListener('click', addToList);
+let shoppingList = document.querySelector("#shopping-cart");
+
+function addToList() {
+    let newItem = document.createElement("li");
+    newItem.textContent = itemToAdd.value;
+
+    let cancelButton = document.createElement("button");
+    cancelButton.textContent = "Remove";
+    cancelButton.style.marginLeft = "1rem";
+    newItem.appendChild(cancelButton);
+    
+    itemToAdd.value = "";
+
+    shoppingList.appendChild(newItem);
+    cancelButton.addEventListener('click', () => shoppingList.removeChild(newItem));
+}
